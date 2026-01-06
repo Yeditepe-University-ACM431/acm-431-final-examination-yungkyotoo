68374486d2875961938b3716bbb3ce87b5433832
yungkyotoo <canmmc15@hotmail.com>
2026-01-06 15:27:09 +0300

    Add composables for login and fake API connection
    Added composables for login screen and fake API connection with arguments.

--- app/src/main/java/com/yeditepe/finalexam/navigation/AppNavGraph.kt
+++ app/src/main/java/com/yeditepe/finalexam/navigation/AppNavGraph.kt
@@ -15,7 +15,13 @@ fun AppNavGraph() {
     ) {
 
         // TODO 1: Add composable for "tasks"
-
+        composable("loginscreen") {LoginScreen(/*...*/)}
+        composable("fakeAPIconnection") {fakeAPIconnection()}
         // TODO 2: Add composable for "taskDetail/{title}"
+        composable(LoginScreen/{userId})
+        arguments= listOf (navArgument ("userId") { type = NavType.stringType })
+
+        composable(fakeAPIconnection/{connected})
+        arguments= listOf (navArgument ("connected") { type = NavType.booleanType })
     }
 }
