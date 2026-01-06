b8cbc650079128da7bca1cd521d81e13a0a91316
yungkyotoo <canmmc15@hotmail.com>
2026-01-06 15:36:19 +0300

    called API
    called API

--- app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
+++ app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
@@ -2,9 +2,11 @@ package com.yeditepe.finalexam.repository
 
 import com.yeditepe.finalexam.api.TaskApi
 import com.yeditepe.finalexam.model.Task
+import jdk.internal.org.jline.utils.ShutdownHooks.Task
 
 class TaskRepository(private val api: TaskApi) {
 
+    @GET("tasks")
     suspend fun fetchTasks(): List<Task> {
         // TODO 1: Call API
         // TODO 2: Convert TaskDto list to Task list
