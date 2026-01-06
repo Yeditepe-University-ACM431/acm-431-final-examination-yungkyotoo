1905e82a5cc1462db27f78071a37e78c5a872b05
yungkyotoo <canmmc15@hotmail.com>
2026-01-06 15:28:50 +0300

    Add sample tasks to TaskViewModel initialization
    Initialize TaskViewModel with a sample list of tasks.

--- app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
@@ -1,13 +1,25 @@
 package com.yeditepe.finalexam.viewmodel
 
 import androidx.lifecycle.ViewModel
+import jdk.internal.org.jline.utils.ShutdownHooks.Task
 
 class TaskViewModel : ViewModel() {
 
     // TODO 1: Create a mutable state list of Task objects
     // Initially add at least 2 tasks
+    init {
+        val samplelist = ListOf(
+            Task(id=1,title=loginscreen ,isCompleted=)
+            Task(id=2,title=fakeAPIconnection, iscompleted=)
+        )
+        Task.values=_samplelist
+    }
 
     fun toggleTask(taskId: Int) {
         // TODO 2: Update isCompleted for the given task
+        var toggleTask: Boolean =
+            RadioButton(toggleTask() onClick= {
+                selected = !selected
+            })
     }
 }
