00288bdc94646eff174526002170e021d2897767
yungkyotoo <canmmc15@hotmail.com>
2026-01-06 15:39:04 +0300

    Update TaskViewModel.kt

--- app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
@@ -9,8 +9,8 @@ class TaskViewModel : ViewModel() {
     // Initially add at least 2 tasks
     init {
         val samplelist = ListOf(
-            Task(id=1,title=loginscreen ,isCompleted=)
-            Task(id=2,title=fakeAPIconnection, iscompleted=)
+            Task(id=1,title="loginscreen" ,isCompleted=yes)
+            Task(id=2,title="fakeAPIconnection", iscompleted=)
         )
         Task.values=_samplelist
     }
